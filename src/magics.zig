a6157ffc950bb5e3f3ecbbab3df046434eb6cce5
Christian Mattes <CarpeNecopinum@noreply.users.github.com>
2021-10-28 10:56:54 +0200

    Drop the formerly commented-out HelloMagic

--- src/magics.zig
+++ src/magics.zig
@@ -2,30 +2,6 @@ const std = @import("std");
 const c = @import("gtk.zig");
 const MagicTrait = @import("magic_trait.zig").MagicTrait;
 
-// const HelloMagic = struct {
-//     x: u64 = 0, // make it a non-zero-bits type
-//     magic = Magic{
-//         .addToMenu = addToMenu
-//     }
-
-//     fn init(allocator: *std.mem.Allocator) !*HelloMagic {
-//         var result = try allocator.create(HelloMagic);
-//         result.* = HelloMagic{};
-//         return result;
-//     }
-
-//     fn hello(item: ?*c.GtkWidget, user_data: ?*c_void) void {
-//         std.debug.print("Hello World!\n", .{});
-//     }
-
-//     pub fn addToMenu(self: @This(), menu: *c.GtkMenu) void {
-//         var item = c.gtk_image_menu_item_new_with_label("Hello World!");
-//         c.gtk_menu_shell_insert(@ptrCast(*c.GtkMenuShell, menu), item, 1);
-//         c.gtk_widget_show(item);
-//         _ = c.g_signal_connect_1(item, "activate", @ptrCast(c.GCallback, HelloMagic.hello), null);
-//     }
-// };
-
 const SingleCommandMagic = struct {
     allocator: *std.mem.Allocator,
     title: [:0]const u8 = "<Unnamed Command>",
@@ -179,7 +155,6 @@ const SubmenuMagic = struct {
 
 pub fn buildMagics(allocator: *std.mem.Allocator) ![]MagicTrait {
     var magics = try allocator.alloc(MagicTrait, 3);
-    //magics[0] = MagicTrait.create(try HelloMagic.init(allocator));
     magics[0] = MagicTrait.create(try SingleCommandMagic.init(allocator, "Hello (Command)", "echo 'Hello World'"));
     magics[1] = MagicTrait.create(try InterruptibleCommandMagic.init(allocator, "Hello (Checkable)", "watch echo 'Hello World'"));
     magics[2] = MagicTrait.create(try SubmenuMagic.init(allocator, "External Monitor", &.{
