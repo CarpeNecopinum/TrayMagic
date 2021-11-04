970d05058bbe367f47f1bf8e02663e5faac233c8
Christian Mattes <CarpeNecopinum@noreply.users.github.com>
2021-11-04 16:11:28 +0100

    Automatically un-check terminated InterruptibleTasks

--- src/magics.zig
+++ src/magics.zig
@@ -53,6 +53,7 @@ pub const InterruptibleCommandMagic = struct {
     title: [:0]const u8 = "<Unnamed Command>",
     command: [*:0]const u8 = "true",
     pid: std.os.pid_t = 0,
+    item: *c.GtkCheckMenuItem = undefined,
 
     pub fn init(allocator: *std.mem.Allocator, title: [:0]const u8, command: [*:0]const u8) !*Self {
         var result = try allocator.create(Self);
@@ -64,32 +65,51 @@ pub const InterruptibleCommandMagic = struct {
         return result;
     }
 
+    fn waitForCompletion(self: *Self) void {
+        const fd = @intCast(std.os.fd_t, @truncate(u32, std.os.linux.pidfd_open(self.pid, 0)));
+        defer _ = std.os.linux.close(fd);
+        std.event.Loop.instance.?.waitUntilFdReadable(fd);
+        std.debug.print("Command `{s}` finished\n", .{self.command});
+
+        c.gtk_check_menu_item_set_active(self.item, 0);
+    }
+
+    fn start(self: *Self) void {
+        const cmd = std.fmt.allocPrintZ(self.allocator, "{s}", .{self.command}) catch return;
+        var args: [3:null]? [*:0]const u8 = undefined;
+        args[0] = "/usr/bin/bash";
+        args[1] = "-c";
+        args[2] = self.command;
+        args[3] = null;
+
+        const envptr = @ptrCast([*:null]const ?[*:0]const u8, std.os.environ.ptr);
+
+        const pid = std.os.fork() catch return;
+        if (pid == 0) {
+            const err = std.os.execvpeZ(args[0].?, &args, envptr);
+            return;
+        } else {
+            self.pid = pid;
+            std.event.Loop.instance.?.runDetached(self.allocator, Self.waitForCompletion, .{self}) 
+                catch return self.stop();
+        }
+    }
+
+    fn stop(self: *Self) void {
+        if (self.pid != 0) {
+                // SIGINT
+            std.os.kill(self.pid, 2) catch |e| {
+                std.debug.print("Can't kill #{}: #{}", .{self.pid, e});
+            };
+        }
+    }
+
     fn toggled(self: *Self, item: *c.GtkCheckMenuItem) callconv(.C) void {
         const onoff = c.gtk_check_menu_item_get_active(item) != 0;
         if (onoff) {
-            const cmd = std.fmt.allocPrintZ(self.allocator, "{s}", .{self.command}) catch return;
-            var args: [3:null]? [*:0]const u8 = undefined;
-            args[0] = "/usr/bin/bash";
-            args[1] = "-c";
-            args[2] = self.command;
-            args[3] = null;
-
-            const envptr = @ptrCast([*:null]const ?[*:0]const u8, std.os.environ.ptr);
-
-            const pid = std.os.fork() catch return;
-            if (pid == 0) {
-                const err = std.os.execvpeZ(args[0].?, &args, envptr);
-                return;
-            } else {
-                self.pid = pid;
-            }
+            self.start();
         } else {
-            if (self.pid != 0) {
-                 // SIGINT
-                std.os.kill(self.pid, 2) catch |e| {
-                    std.debug.print("Can't kill #{}: #{}", .{self.pid, e});
-                };
-            }
+            self.stop();
         }
     }
 
@@ -104,6 +124,7 @@ pub const InterruptibleCommandMagic = struct {
             @ptrCast(c.GCallback, Self.toggled), 
             @ptrCast(*c_void, self)
             );
+        self.item = @ptrCast(*c.GtkCheckMenuItem, item);
     }
 };
 
